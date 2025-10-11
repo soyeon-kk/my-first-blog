babeaa8957b33774cb9a327d74a3d4f90078e35a
arsenic <sy040103@khu.ac.kr>
2025-10-11 11:49:53 +0900

    feat: Display images on post

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {}) # blog/post_list.html 템플릿을 보여준다는 의미
+    posts = Post.objects.all().order_by('-published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts}) # blog/post_list.html 템플릿을 보여준다는 의미
